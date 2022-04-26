8013c1660791626e1084b0675095ba5eda796b47
LaupWing <loc_strict@hotmail.com>
2022-04-26 17:00:55 +0200

    dupa

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-        _mint(msg.sender, initialSupply);
-    }
+   constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+      _mint(msg.sender, initialSupply);
+   }
 }
